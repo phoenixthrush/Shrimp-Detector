752b4183d1c4822fb9d1d25ce83a016cf09605b3
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-01-23 10:26:55 +0100

    upload src

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 all:
+	xxd -i site/index.html > html.c
+
 	@cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release
 	@cmake --build build
+
+	strip ./build/bin/example
+	./build/bin/example
 
 clean:
 	@rm -rf build
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "webview/webview.h"
 #include <stddef.h>
+#include "html.c"
 
 #ifdef _WIN32
 #include <windows.h>
@@ -7,19 +8,21 @@
 
 #ifdef _WIN32
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine,
-                   int nCmdShow) {
-  (void)hInst;
-  (void)hPrevInst;
-  (void)lpCmdLine;
-  (void)nCmdShow;
+                   int nCmdShow)
+{
+    (void)hInst;
+    (void)hPrevInst;
+    (void)lpCmdLine;
+    (void)nCmdShow;
 #else
-int main(void) {
+int main(void)
+{
 #endif
-  webview_t w = webview_create(0, NULL);
-  webview_set_title(w, "Basic Example");
-  webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
-  webview_set_html(w, "Thanks for using webview!");
-  webview_run(w);
-  webview_destroy(w);
-  return 0;
+    webview_t w = webview_create(0, NULL);
+    webview_set_title(w, "Shrimp Detector");
+    webview_set_size(w, 700, 500, WEBVIEW_HINT_FIXED);
+    webview_set_html(w, (const char *)site_index_html);
+    webview_run(w);
+    webview_destroy(w);
+    return 0;
 }
